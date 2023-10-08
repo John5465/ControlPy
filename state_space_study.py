899056d4e2398c9d75ec33a6cd41320cfd1ff00d
John <364064698@qq.com>
2023-10-08 21:47:39 +0800

    add state_space study script

---
+++ state_space_study.py
@@ -0,0 +1,22 @@
+import sympy
+from sympy import init_session
+init_session(quiet=True) 
+
+t, s = sympy.symbols('t s')
+x = sympy.Function('x')
+m, b, k = sympy.symbols('m b k')
+
+# 弹簧阻尼系统
+f = m * x(t).diff(t).diff(t) + b*x(t).diff(t) + k*x(t)
+
+f_lap = sympy.laplace_transform(f, t, s)
+
+#f_lap = sympy.simplify(f_lap)
+
+# 传递函数 X(s) / F(s)
+Gs = 1 / (m * s**2 + b * s + k)
+
+z1 = x(t)
+z2 = z1.diff(t)
+
+z2
